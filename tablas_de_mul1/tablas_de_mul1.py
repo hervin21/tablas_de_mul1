d14fd3bbefd2e2505f71a34bf48b4cb6cd39fadd
hervin21 <142458280+hervin21@users.noreply.github.com>
2023-10-29 22:29:28 -0600

    Agregar archivos de proyecto.

---
+++ tablas_de_mul1/tablas_de_mul1.py
@@ -0,0 +1,16 @@
+NUM = int(input("CUANTAS TABLAS: "))
+
+T = 1
+
+while T <= NUM:
+    I = 10
+
+    while I >= 1:
+        RESUL = T * I
+        print(f"{T} x {I} = {RESUL}")
+        I = I - 1
+
+    T = T + 1
+
+input("Pulse una tecla para continuar")
+
